f1f47b37f858524572ee11e3934c7d6a62a3a5e5
Aurelia Natasha <aurelianatashaaa@gmail.com>
2021-11-27 13:14:46 +0700

    add Next button

--- ProjectTest/ViewController.swift
+++ ProjectTest/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func nextBtnPressed(_ sender: Any) {
+        print("Going to the next page")
+    }
+    
 
 }
 
